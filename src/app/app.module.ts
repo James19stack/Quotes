0962888a827e071e7be84cf46a5a133064588f33
jefferson <mirarajames29@gmail.com>
2020-08-24 10:44:15 +0300

    Add pipe custom-date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { QuotesDetailsComponent } from './quotes-details/quotes-details.componen
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { VotesDirective } from './votes.directive';
+import { CustomDatePipe } from './custom-date.pipe';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { VotesDirective } from './votes.directive';
     QuotesDetailsComponent,
     QuoteFormComponent,
     QuoteDetailsComponent,
-    VotesDirective
+    VotesDirective,
+    CustomDatePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/custom-date.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { NgAnalyzedFile } from '@angular/compiler';
+
+@Pipe({
+  name: 'customDate'
+})
+export class CustomDatePipe implements PipeTransform {
+
+  transform(value: any): any {
+    let date:Date=new Date();
+    let updatedDate:any= new Date(date.getFullYear(),date.getMonth(),date.getDate());
+    let dateDiffernce=Math.abs(updatedDate-value);
+    const secondsInDay=86400;
+    let dateDiffernceInSeconds=dateDiffernce*0.001;
+    let dateCount=dateDiffernceInSeconds/secondsInDay;
+    
+    if(dateCount>=0.5){
+      return Math.ceil(dateCount)+" "+"days ago.";
+    } else{
+      return "today.";
+    }
+    
+  }
+
+}
